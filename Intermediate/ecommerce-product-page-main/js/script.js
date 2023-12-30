0b4f04fa1af07e80da50ebeee11d170848435e06
Amirhossein <4mirh03einhashemi@gmail.com>
2023-12-30 23:06:48 +0330

    create mobile navigation

--- Intermediate/ecommerce-product-page-main/js/script.js
+++ Intermediate/ecommerce-product-page-main/js/script.js
@@ -1,5 +1,13 @@
+const darkOverlay = document.querySelector(".overlay");
+
 const carousel = document.querySelector(".carousel");
 const slides = document.querySelectorAll(".carousel__slide");
+
+const navBtn = document.querySelector(".nav__btn");
+const navIcon = document.querySelectorAll(".nav__btn img");
+const navMenu = document.querySelector(".nav__list");
+// const hamIcon = document.querySelector('.nav__hamburger');
+// const closeIcon = document.querySelector('.nav__close');
 
 // carousel
 const maxSlide = slides.length - 1;
@@ -35,4 +43,11 @@ carousel.addEventListener("click", (e) => {
   if (!clicked) return;
 
   move(clicked);
+});
+
+// mobile menu
+navBtn.addEventListener("click", function () {
+  navIcon.forEach((icon) => icon.classList.toggle("active"));
+  navMenu.classList.toggle("open");
+  darkOverlay.classList.toggle("block");
 });
