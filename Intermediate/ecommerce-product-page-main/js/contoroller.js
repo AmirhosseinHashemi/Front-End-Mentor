b51bc450f05814a72c0ab2fbe08b167028f1ea0f
Amirhossein <4mirh03einhashemi@gmail.com>
2024-01-04 19:35:18 +0330

    implement slider functionality

--- Intermediate/ecommerce-product-page-main/js/contoroller.js
+++ Intermediate/ecommerce-product-page-main/js/contoroller.js
@@ -1,5 +1,5 @@
 import Counter from "./views/counter.js";
-import Slider from "./views/slider.js";
+import { Slider, Lightbox } from "./views/slider.js";
 
 // handle counter
 const countControl = function (go) {
@@ -7,13 +7,49 @@ const countControl = function (go) {
 };
 
 // handle slider in mobile
-const sliderControl = function (state) {
-  Slider.move(state);
-  Slider.render(state);
+const sliderControl = function (element) {
+  const attValue = element.dataset.go;
+  Slider.render(attValue);
+};
+
+// handle slider in for lightbox
+const sliderLightboxControl = function (element) {
+  const attValue = element.dataset.go;
+  Lightbox.render(attValue);
+};
+
+// handle thumbnail image
+const thumbControl = function (element) {
+  const attValue = element.dataset.thumbnail;
+  Slider.render(attValue);
+};
+
+// handle thumbnail image for lightbox
+const thumbLightboxControl = function (element) {
+  const attValue = element.dataset.thumbnail;
+  Lightbox.render(attValue);
+};
+
+// handle open lightbox
+const lightboxOpenControl = function (element) {
+  const attValue = element.dataset.lightbox;
+  if (!attValue || window.innerWidth < 768) return;
+
+  Lightbox.open();
+};
+
+// handle close lightbox
+const lightboxCloseControl = function () {
+  Lightbox.close();
 };
 
 const init = function () {
   Counter.addHandlerClick(countControl);
   Slider.addHandlerClick(sliderControl);
+  Slider.addhandlerThumb(thumbControl);
+  Slider.addhandlerSlide(lightboxOpenControl);
+  Lightbox.addHandlerClick(sliderLightboxControl);
+  Lightbox.addhandlerThumb(thumbLightboxControl);
+  Lightbox.addhandlerClose(lightboxCloseControl);
 };
 init();
--- Intermediate/ecommerce-product-page-main/js/views/slider.js
+++ Intermediate/ecommerce-product-page-main/js/views/slider.js
@@ -13,7 +13,25 @@ class SliderView {
 
   addHandlerClick(handler) {
     this._parent.addEventListener("click", function (e) {
-      const clicked = e.target.closest(".carousel__btn")?.dataset.go;
+      const clicked = e.target.closest(".carousel__btn");
+      if (!clicked) return;
+
+      handler(clicked);
+    });
+  }
+
+  addhandlerThumb(handler) {
+    this._parent.addEventListener("click", function (e) {
+      const clicked = e.target.closest(".carousel__thumbnail button");
+      if (!clicked) return;
+
+      handler(clicked);
+    });
+  }
+
+  addhandlerSlide(handler) {
+    this._parent.addEventListener("click", function (e) {
+      const clicked = e.target.closest(".carousel__slide");
       if (!clicked) return;
 
       handler(clicked);
@@ -34,18 +52,18 @@ class SliderView {
     else this._currentSlide--;
   }
 
-  move(state) {
-    if (state === "next") this._next();
-    if (state === "previous") this._previous();
-  }
-
   _generateMarkup() {
+    if (this._data === "next") this._next();
+    if (this._data === "previous") this._previous();
+    if (Number.isFinite(+this._data)) this._currentSlide = +this._data;
     return `
     <!-- slider -->
     <div class="carousel__slider" aria-label="slider">
       <div class="carousel__slides-container">
         <figure class="carousel__slide" data-lightbox="true">
-          <img src="./images/image-product-${this._currentSlide}.jpg" alt="shoe image${this._currentSlide}" />
+          <img src="./images/image-product-${
+            this._currentSlide
+          }.jpg" alt="shoe image${this._currentSlide}" />
         </figure>
       </div>
 
@@ -68,39 +86,43 @@ class SliderView {
 
     <!-- thumbnail __ for desktop -->
     <div class="carousel__thumbnail" aria-label="thumbnail">
-      <button type="button">
+      <button class=${
+        +this._data === 1 ? "active-btn" : ""
+      } type="button" data-thumbnail="1">
         <img
           class="carousel__thumb-img"
           src="./images/image-product-1-thumbnail.jpg"
           alt="thumbnail product 1"
-          data-thumbnail="1"
         />
       </button>
 
-      <button type="button">
+      <button class=${
+        +this._data === 2 ? "active-btn" : ""
+      } type="button" data-thumbnail="2">
         <img
           class="carousel__thumb-img"
           src="./images/image-product-2-thumbnail.jpg"
           alt="thumbnail product 2"
-          data-thumbnail="2"
         />
       </button>
 
-      <button type="button">
+      <button class=${
+        +this._data === 3 ? "active-btn" : ""
+      } type="button" data-thumbnail="3">
         <img
           class="carousel__thumb-img"
           src="./images/image-product-3-thumbnail.jpg"
           alt="thumbnail product 3"
-          data-thumbnail="3"
         />
       </button>
 
-      <button type="button">
+      <button class=${
+        +this._data === 4 ? "active-btn" : ""
+      } type="button" data-thumbnail="4">
         <img
           class="carousel__thumb-img"
           src="./images/image-product-4-thumbnail.jpg"
           alt="thumbnail product 4"
-          data-thumbnail="4"
         />
       </button>
     </div>
@@ -108,4 +130,123 @@ class SliderView {
   }
 }
 
-export default new SliderView();
+class LightboxView extends SliderView {
+  _parent = document.querySelector(".lightbox");
+
+  open() {
+    this._parent.style.display = "flex";
+  }
+
+  close() {
+    this._parent.style.display = "none";
+  }
+
+  addhandlerClose(handler) {
+    this._parent.addEventListener("click", function (e) {
+      const clicked = e.target.closest(".close");
+      if (!clicked) return;
+      handler();
+    });
+  }
+
+  _generateMarkup() {
+    if (this._data === "next") this._next();
+    if (this._data === "previous") this._previous();
+    if (Number.isFinite(+this._data)) this._currentSlide = +this._data;
+    return `
+    <div class="content">
+    <button type="button" class="close">
+      <svg
+        class="close-icon"
+        aria-label="close icon"
+        width="14"
+        height="15"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="m11.596.782 2.122 2.122L9.12 7.499l4.597 4.597-2.122 2.122L7 9.62l-4.595 4.597-2.122-2.122L4.878 7.5.282 2.904 2.404.782l4.595 4.596L11.596.782Z"
+          fill="#69707D"
+          fill-rule="evenodd"
+        />
+      </svg>
+    </button>
+    <div class="carousel" aria-label="carousel">
+      <!-- slider -->
+      <div class="carousel__slider" aria-label="slider">
+        <div class="carousel__slides-container">
+          <figure class="carousel__slide">
+            <img src="./images/image-product-${
+              this._currentSlide
+            }.jpg" alt="shoe image${this._currentSlide}" />
+          </figure>
+        </div>
+
+        <button
+          class="carousel__btn carousel__btn--previous"
+          data-go="previous"
+          type="button"
+        >
+          <img src="./images/icon-previous.svg" alt="previous" />
+        </button>
+
+        <button
+          class="carousel__btn carousel__btn--next"
+          data-go="next"
+          type="button"
+        >
+          <img src="./images/icon-next.svg" alt="next" />
+        </button>
+      </div>
+
+      <!-- thumbnail __ for desktop -->
+      <div class="carousel__thumbnail" aria-label="thumbnail">
+        <button class=${
+          +this._data === 1 ? "active-btn" : ""
+        } type="button" data-thumbnail="1">
+          <img
+            class="carousel__thumb-img"
+            src="./images/image-product-1-thumbnail.jpg"
+            alt="thumbnail product 1"
+          />
+        </button>
+
+        <button class=${
+          +this._data === 2 ? "active-btn" : ""
+        } type="button" data-thumbnail="2">
+          <img
+            class="carousel__thumb-img"
+            src="./images/image-product-2-thumbnail.jpg"
+            alt="thumbnail product 2"
+          />
+        </button>
+
+        <button class=${
+          +this._data === 3 ? "active-btn" : ""
+        } type="button" data-thumbnail="3">
+          <img
+            class="carousel__thumb-img"
+            src="./images/image-product-3-thumbnail.jpg"
+            alt="thumbnail product 3"
+          />
+        </button>
+
+        <button class=${
+          +this._data === 4 ? "active-btn" : ""
+        } type="button" data-thumbnail="4">
+          <img
+            class="carousel__thumb-img"
+            src="./images/image-product-4-thumbnail.jpg"
+            alt="thumbnail product 4"
+          />
+        </button>
+      </div>
+    </div>
+  </div>
+    `;
+  }
+}
+
+export const Slider = new SliderView();
+export const Lightbox = new LightboxView();
+
+// export default new SliderView();
